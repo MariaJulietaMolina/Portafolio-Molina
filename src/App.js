85652ae0e4b61780d9be46585a6ff921f923dcb3
MariaJulietaMolina <mariajulietamolina91@gmail.com>
2025-11-29 18:48:57 -0300

    Portfolio preventivo jaja

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './selfie.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aqui va el portfolio
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://mariajulietamolina.github.io/cvfullstackmariajulietamolina/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fullstack developer TRAINEE
+
+
         </a>
       </header>
     </div>
